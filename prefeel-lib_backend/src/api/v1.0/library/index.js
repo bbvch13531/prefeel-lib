73bebd1579a79204ef6ae6d6a0de64677a898581
oh3vci <oh3vci@gmail.com>
2018-01-05 16:36:59 +0900

    Add library api 'getSelectedLibrary'

--- prefeel-lib_backend/src/api/v1.0/library/index.js
+++ prefeel-lib_backend/src/api/v1.0/library/index.js
@@ -4,5 +4,6 @@ import * as ctrl from './library.ctrl.js';
 const router = express.Router();
 
 router.get('/', ctrl.getLibraries);
+router.get('/:library-name', ctrl.getSelectedLibrary);
 
 export default router;
