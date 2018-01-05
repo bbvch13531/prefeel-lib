edaef157c6d46ead1b8b616df01a35f550d58180
oh3vci <oh3vci@gmail.com>
2018-01-05 22:38:05 +0900

    Add 'getSelectedLibrary' api for showing library information

--- prefeel-lib_backend/src/api/v1.0/library/index.js
+++ prefeel-lib_backend/src/api/v1.0/library/index.js
@@ -4,5 +4,6 @@ import * as ctrl from './library.ctrl.js';
 const router = express.Router();
 
 router.get('/', ctrl.getLibraries);
+router.get('/:library_name', ctrl.getSelectedLibrary);
 
 export default router;
--- prefeel-lib_backend/src/api/v1.0/library/library.ctrl.js
+++ prefeel-lib_backend/src/api/v1.0/library/library.ctrl.js
@@ -3,14 +3,17 @@ import Library from './../../../db/models/library';
 // GET /api/v1.0/library
 export const getLibraries = (req, res) => {
   Library.find((err, libraries) => {
-    if (err) return res.status(500).send({error: 'database failure'});
+    if (err) return res.status(500).json({error: err});
     res.json(libraries);
   });
 };
 
-// GET /api/v1.0/library/:libraryName
-/*
-export const getSelectedLibrary = async (req, res) => {
-  const library = await Library.findLibrary(req.params.name);
+// GET /api/v1.0/library/:library_name
+export const getSelectedLibrary = (req, res) => {
+  console.log(req.params);
+  Library.findOne({name: req.params.library_name}, (err, library) => {
+    if (err) return res.status(500).json({error: err});
+    if (!library) return res.status(404).json({error: 'library not found'});
+    res.json(library);
+  });
 };
-*/
