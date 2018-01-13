38bbf2a79ad427964cc1f2114e1a15f2861a2cc2
WoochanLee <dldncks93@gmail.com>
2018-01-13 17:58:13 +0900

    re-design project model

--- prefeel-lib_backend/src/api/v1.0/project/project.ctrl.js
+++ prefeel-lib_backend/src/api/v1.0/project/project.ctrl.js
@@ -3,7 +3,7 @@ import Directory from './../../../db/models/project';
 // GET /api/v1.0/directory/:directory_name
 export const getSelectedProject = (req, res) => {
   console.log(req.params);
-  Directory.findOne({projectName: req.params.project_name}, (err, project) => {
+  Directory.findOne({directoryName: req.params.project_name}, (err, project) => {
     if (err) return res.status(500).json({error: err});
     if (!project) return res.status(404).json({error: 'project not found'});
     res.json(project);
