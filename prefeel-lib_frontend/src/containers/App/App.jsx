a0261da286057c3be00456d4718d43a97a10d6aa
Kyungyoung Heo <bbvch13531@gmail.com>
2018-01-21 13:46:56 +0900

    Feature server http communication

--- prefeel-lib_frontend/src/containers/App/App.jsx
+++ prefeel-lib_frontend/src/containers/App/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import { hot } from 'react-hot-loader';
+import Axios from 'axios';
+
 import styles from './App.scss';
 
 import Header from 'containers/Header';
@@ -9,6 +11,23 @@ import PracticePage from 'containers/PracticePage';
 import ProjectPage from 'containers/ProjectPage';
 
 class App extends React.Component {
+
+  componentDidMount(){
+    return Axios({
+      method: 'get',
+      url: 'http://52.78.136.70:4000/api/v1.0/Library',
+    })
+
+    .then(async (response) => {
+      let resdata=response.data;
+      console.log(resdata);
+    })
+
+    .catch((error) =>{
+      console.log("error: "+error);
+    })
+  }
+
   render() {
     return (
       <div className='App'>
