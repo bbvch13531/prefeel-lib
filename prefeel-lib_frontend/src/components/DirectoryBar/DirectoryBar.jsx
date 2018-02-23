592c3c6a92647008dee1f1899e067ad6367cc1d2
oh3vci <oh3vci@gmail.com>
2018-02-23 23:05:23 +0900

    Modify styles of directory bar & linter bar

--- prefeel-lib_frontend/src/components/DirectoryBar/DirectoryBar.jsx
+++ prefeel-lib_frontend/src/components/DirectoryBar/DirectoryBar.jsx
@@ -1,10 +1,72 @@
 import React from 'react';
+import styles from './DirectoryBar.scss';
 
 const DirectoryBar = ({isOpen}) => {
   const directoryBarClassName = isOpen ? 'directory-bar open' : 'directory-bar';
+  /*
+  const example = {
+    "folderName": "react_tutorial-master",
+    "file": [
+            {
+                    "fileName": "README.md",
+                    "content": "text"
+            },
+            {
+                    "fileName": "package-lock.json",
+                    "content": "text"
+            },
+            {
+                    "fileName": "package.json",
+                    "content": "text"
+            }
+    ],
+    "folder": [
+            {
+                    "folderName": "public",
+                    "file": [
+                            {
+                                    "fileName": "index.html",
+                                    "content": "text"
+                            },
+                            {
+                                    "fileName": "manifest.json",
+                                    "content": "text"
+                            }
+                    ],
+                    "folder": []
+            },
+            {
+                    "folderName": "react_tutorial",
+                    "file": [],
+                    "folder": []
+            },
+            {
+                    "folderName": "src",
+                    "file": [
+                            {
+                                    "fileName": "index.css",
+                                    "content": "text"
+                            },
+                            {
+                                    "fileName": "index.js",
+                                    "content": "text"
+                            }
+                    ],
+                    "folder": []
+            }
+    ]
+  };
+
+  makeTree = () => {
+    const tree = example;
+  }
+  */
+
   return (
     <div className={directoryBarClassName}>
-
+      <div className="project-name">
+        directoryBar
+      </div>
     </div>
   );
 }
--- prefeel-lib_frontend/src/components/LinterBar/LinterBar.jsx
+++ prefeel-lib_frontend/src/components/LinterBar/LinterBar.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import styles from './LinterBar.scss';
 
 const LinterBar = ({isOpen}) => {
   const linterBarClassName = isOpen ? 'linter-bar open' : 'linter-bar';
   return (
     <div className={linterBarClassName}>
-
+      LinterBar
     </div>
   );
 }
